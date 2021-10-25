8cdfbf309086a5aa8842f3294e47c9bd5159d373
sonnetk <kara4iowa.tan@yandex.ru>
2021-10-25 15:59:04 +0300

    iter6

--- iter6.js
+++ iter6.js
@@ -41,6 +41,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 //    - сохранение Cookie и перезапись с параметрами
+    document.querySelector('#clickCookie').onclick = () => {
+
+        let name = "my name";
+        let value = "Tanya K"
+        document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+        console.log(document.cookie); // my%20name=Tanya%20K
+
+        document.cookie = "path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT"
+        console.log(document.cookie)
+    }
+
+    //https://learn.javascript.ru/cookie Консоль:
+    // document.cookie
+    //'_ga=GA1.2.1772461389.1632993166; _ym_uid=163299316610068329; _ym_d=1632993166; vblastvisit=1634726449; ... user=John'
+// Это сторонние файлы cookie, размещенные Google (Позволяет различать пользователей)
+// Яндексом (Используется для идентификации пользователя)
+// Дата первой пользовательской сессии (Яндекс)
+
+    //document.cookie='user=Tanya'
+    //'user=Tanya'
+    //document.cookie
+    //'_ga=GA1.2.1772461389.1632993166; _ym_uid=163299316610068329; _ym_d=1632993166; vblastvisit=1634726449; ... user=Tanya'
+
+
 //    - сохранение данных в Local Storage, Session Storage и получение данных из них
 //    - debugging в консоли
 
